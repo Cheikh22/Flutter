239fead21b69665c4e4a5975bae8304eb1b834c9
cheikh22 <cheikh.raby24@gmail.com>
2021-12-09 11:41:32 +0000

    flutter project

---
+++ Notes/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Notes'),
+        ),
+        body: Column(children: [
+          Text('The questions!'),
+          RaisedButton(
+            child: Text('Answer 1'),
+            onPressed: null,
+          ),
+          RaisedButton(
+            child: Text('Answer 2'),
+            onPressed: null,
+          ),
+          RaisedButton(
+            child: Text('Answer 3'),
+            onPressed: null,
+          ),
+        ]),
+      ),
+    );
+  }
+}

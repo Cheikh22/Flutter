55491c7ddeac67b45f8edcfd07c2542717fd4429
cheikh22 <cheikh.raby24@gmail.com>
2021-12-10 11:40:10 +0000

    backend crud

--- Backend/main.py
+++ Backend/main.py
@@ -1,6 +1,5 @@
-from fastapi import FastAPI ,Depends
-from sqlalchemy import engine
-from sqlalchemy.orm import Session
+from fastapi import FastAPI, Depends, status, Response, HTTPException
+from sqlalchemy.orm import Session, synonym
 import schemas
 from database import engine , SessionLocal
 import models
@@ -16,11 +15,8 @@ def get_db():
     finally:
         db.close()
 
-@app.get("/")
-def root():
-    return {"message": "Hello World"}
-    
-@app.post("/user")
+
+@app.post("/user", status_code=status.HTTP_201_CREATED)
 def create_user(request: schemas.User,db :Session = Depends(get_db)):
     new_user =models.User(name=request.name,email=request.email,password=request.password)
     db.add(new_user)
@@ -28,18 +24,28 @@ def create_user(request: schemas.User,db :Session = Depends(get_db)):
     db.refresh(new_user)
     return new_user
 
-@app.get("/user/{id}")
-def read_user(id: int):
-    return f"read user with id {id}"
-
-@app.put("/user/{id}")
-def update_user(id: int):
-    return f"update user with id {id}"
-
-@app.delete("/user/{id}")
-def delete_user(id: int):
-    return f"delete user with id {id}"
-
 @app.get("/user")
-def read_user_list():
-    return "read userlist"
+def read_user_list(db :Session = Depends(get_db)):
+    users = db.query(models.User).all()
+    return users
+
+@app.get("/user/{id}")
+def read_user(id, db :Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"User with the id {id} is not available"
+        )
+    return user
+
+@app.put("/user/{id}", status_code=status.HTTP_202_ACCEPTED)
+def update_user(id, request: schemas.User,db :Session = Depends(get_db)):
+    db.query(models.User).filter(models.User.id == id).update(request.__dict__)
+    db.commit()
+    return 'done'
+
+@app.delete("/user/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_user(id, db :Session = Depends(get_db)):
+    db.query(models.User).filter(models.User.id == id).delete(synchronize_session=False)
+    db.commit()
+    return f"User with id {id} is deleted"

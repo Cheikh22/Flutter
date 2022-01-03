59c093e2102823a0dd4e8b05e2ed0f90091f7d86
Cheikh <cheikh.raby24@gmail.com>
2022-01-03 09:44:43 +0000

    Moving task and note project to other folders

--- Notes/notes/lib/constants/string.dart
+++
@@ -1,3 +0,0 @@
-const HOME_ROUTE = '/';
-const ADD_NOTE_ROUTE ="/add_note";
-const EDIT_NOTE_ROUTE = "/edit_note";
--- Notes/notes/lib/data/Services/Services.dart
+++
@@ -1,9 +0,0 @@
-import 'package:notes/data/Services/DatabaseHelper.dart';
-import 'package:notes/data/models/notes.dart';
-
-class Services {
-  
-  void fetchNotes() async{
-  }
-  
-}
